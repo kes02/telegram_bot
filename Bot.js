42c99b0b7d570e9b4bca76a0ff657a29312f5957
kes02 <hijin1220@naver.com>
2023-03-06 02:34:49 +0900

    쉼표 제거 및 new line 해결

--- Bot.js
+++ Bot.js
@@ -2,7 +2,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const cheerio = require('cheerio');
-//const { data } = require('cheerio/lib/api/attributes');
 const log = console.log;
 
 const getHtml = async () => {
@@ -13,8 +12,7 @@ const getHtml = async () => {
     }
 };
 
-// `botFather`가 제공한 `token`으로 API 통신에 사용한다
-const token= '6150005852:AAEygUCZViVfC0SiUEY74b69sFQqPwB2ST0'; // <--- 나의 Token
+const token = '6150005852:AAEygUCZViVfC0SiUEY74b69sFQqPwB2ST0'; 
 const chatId = '1877133528';
 // 새로운 'bot' 인스턴스를 생성해 'polling'으로 업데이트를 fetch 하게 한다
 const bot = new TelegramBot(token, {polling: true});
@@ -27,65 +25,31 @@ getHtml()
         //const $bodyList = $("tbody tr").children("td.b-td-left"); //tbody -> tr -> td.b-td-left
         const $bodyList = $("tbody tr"); //tbody -> tr -> td.b-td-left
         // if not tbody -> tr.b-top-box
-
-        $bodyList.each(function(i, elem) {
+        //0. 기존의 쉼표 -> | 로 바꾸고 쉼표 -> 지우고, | -> 쉼표로 다시 바꿈
+        //1. number랑 title 맨뒤 공백 삭제
+        $bodyList.each(function(i, elem) { //동기
             ulList[i] = {
-                number: $(this).find('td.b-num-box').text().replace(/[\t\r\n\v\f\s]+/g, "").replace(/[\s\uFEFF\xA0]+$/gi,''),
-                //title: $(this).find('div.b-title-box a').text().replace(/[\t\r\n\v\f]+/g, "").replace("[공지]",""), //a 태그 안에서 title 속성 갖고 있음
-                title: $(this).find('div.b-title-box a').attr('title').replace(/[\t\r\n\v\f]+/g, "").replace(/자세히 보기/gi,"").replace(/[\s\uFEFF\xA0]+$/gi,''), //a 태그 안에서 title 속성 갖고 있음
+                number: $(this).find('td.b-num-box').text().replace(/[\t\r\n\v\f]+/g, "").replace(/[\s\uFEFF\xA0]+$/gi,''),
+                title: $(this).find('div.b-title-box a').attr('title').replace(/[\t\r\n\v\f]+/g, "").replace(/자세히 보기/gi,"").replace(/[\s\uFEFF\xA0]+$/gi,'').replace(/,/gi, '|'), //a 태그 안에서 title 속성 갖고 있음
                 url: "https://www.ajou.ac.kr/kr/ajou/notice.do" + $(this).find('div.b-title-box a').attr('href'),
                 division: $(this).find('span.b-cate').text().replace(/[\t\r\n\v\f]+/g, ""), //분류
                 writer: $(this).find('span.b-writer').text().replace(/[\t\r\n\v\f]+/g, ""), //작성팀
                 date: $(this).find('span.b-date').text().replace(/[\t\r\n\v\f]+/g, "") //날짜
-                //image_url: $(this).find('p.poto a img').attr('src'),
-                //image_alt: $(this).find('p.poto a img').attr('alt'),
-                //summary: $(this).find('p.lead').text().slice(0, -11),  
             };
-            //.chtext = String(ulList[i])
-            //const chtext = JSON.stringify(ulList[i], null, '\n').replace(/,$/g, "\n");
             //const chtext = JSON.stringify(ulList[i], null, '\n').replace(/[\"\\{\\}]/gi, "");  //대대괄호, 따음표 제거
-            //const chtext = JSON.stringify(ulList[i], null, '\n').replace(/[\t\r\n\v\f]{3,}|\s{3,}/g, "|").split("|");
-            bot.sendMessage(chatId, chtext);
+            const chtext = JSON.stringify(ulList[i]).replace(/[\"\\{\\}]/gi, "").replace(/,/gi, "\n").replace(/[|]/gi, ',');  //대대괄호, 따음표 제거
+            
+            //let result = await bot.sendMessage(chatId, chtext); //Bot
+            //console.log(result)
+            setTimeout(() => {
+                bot.sendMessage(chatId, chtext);
+              }, i*1000); // Delay each message by 1 second
+
           });
       
           const data = ulList.filter(n => n.title);
           
           return data;
         })
-        .then(res => {
-            log(res)
-            
-        });
+        .then(res => log(res));
         
-
-// 정규식으로 '/echo'를 판별하고 그 뒤에 어떤 메시지든 'msg'에 담는다
-// bot.onText(/\/echo (.+)/, (msg, match) => {   
-//     const chatId = msg.chat.id;
-//     const resp = "꺄악: "+match[1]; 
-//     // 식별된 "msg"는 보내온 채팅방('chatId')에게 앵무새처럼 재전송한다 ("꺄악: 'msg'")
-//     bot.sendMessage(chatId, resp);
-//  });
-
-//  bot.on('message', (msg) => {
-//      const chatId = msg.chat.id;
-    
-//      // send a message to the chat acknowledging receipt of their message
-//      bot.sendMessage(chatId, 'Received your message');
-// });
-
-// 정규식으로 '/a'를 판별하고 그 뒤에 어떤 메시지든 'msg'에 담는다
-// bot.onText(\/a\  => {   
-//     chatId = '1877133528';
-
-    
-
-
-//     bot.sendMessage(chatId, data);
-//  });
-
-//  bot.on('message', (msg) => {
-//      const chatId = msg.chat.id;
-    
-//      // send a message to the chat acknowledging receipt of their message
-//      bot.sendMessage(chatId, 'Received your message');
-// });
