ad621fa77973c7d7e809d920e5e555215d7aec20
kes02 <hijin1220@naver.com>
2023-03-12 20:59:28 +0900

    수정전 단계

--- Bot.js
+++ Bot.js
@@ -32,30 +32,34 @@ getHtml()
                 title: $(this).find('div.b-title-box a').attr('title').replace(pattern, "").replace(/자세히 보기/gi,"").replace(back_pattern,'').replace(/,/gi, '|'), //a 태그 안에서 title 속성 갖고 있음
                 url: "https://www.ajou.ac.kr/kr/ajou/notice.do" + $(this).find('div.b-title-box a').attr('href'),
                 division: $(this).find('span.b-cate').text().replace(pattern, ""), //분류
+                new: $(this).find('div.b-title-box div.b-etc-box p.b-new span').text(),
                 writer: $(this).find('span.b-writer').text().replace(pattern, ""), //작성팀
                 date: $(this).find('span.b-date').text().replace(pattern, "") //날짜
             };
-            //bot이 /공지사항이라는 명령어를 입력 받았을 때, 해당 메시지를 입력 받았을때 출력
-            bot.onText(/\/전체공지/, (msg) => {
-                const chtext = JSON.stringify(ulList[i]).replace(/[\"\\{\\}]/gi, "").replace(/,/gi, "\n").replace(/[|]/gi, ',');  //대대괄호, 따음표 제거
-                if(infotext.includes('공지') == false){ //fix된 공지가 아닌 것만 전송 받음
+
+
+            log(ulList[i])
+            const infotext = JSON.stringify(ulList[i]).replace(/[\"\\{\\}]/gi, "").replace(/,/gi, "\n").replace(/[|]/gi, ',');  //대대괄호, 따음표 제거
+
+            bot.onText(/\/New/, (msg) => {
+                if(infotext.includes('NEW')){ //fix된 공지가 아닌 것만 전송 받음
                     setTimeout(() => {
                         bot.sendMessage(chatId, infotext);
                     }, i*1000); // Delay each message by 1 second
                 }
-            });
-            //bot이 /공지라는 명령어를 입력 받았을 때, 해당 메시지를 입력 받았을때 출력
+            }); //bot이 /New라는 명령어를 입력 받았을 때, 해당 메시지를 입력 받았을때 출력
+
             bot.onText(/\/공지/, (msg) => {
-                const infotext = JSON.stringify(ulList[i]).replace(/[\"\\{\\}]/gi, "").replace(/,/gi, "\n").replace(/[|]/gi, ',');  //대대괄호, 따음표 제거
                 if(infotext.includes('공지')){
                     setTimeout(() => {
                         bot.sendMessage(chatId, infotext);
                     }, i*1000); // Delay each message by 1 second
                 }
-            });
+            }); //bot이 /공지라는 명령어를 입력 받았을 때, 해당 메시지를 입력 받았을때 출력
         });
           const data = ulList.filter(n => n.title);
           return data;
         })
-        .then(res => log(res));
+        //.then(res => log(res));
 
+// 각각의 parsing method 추가 -> string
